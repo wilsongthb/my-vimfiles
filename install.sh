d69bb14a087818d871ebbf5b8df31eb78ffc89a5
Wilson <wilsonaux1@gmail.com>
2020-03-19 17:08:44 -0500

    cambios al uso de repositorios

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 echo 'Begin install'
 
+# VIM AUTO SAVE
+git clone https://github.com/vim-scripts/vim-auto-save.git ~/.vim/vim-scripts/vim-auto-save
+
+# XOLOX MISC AND SESSION
+git clone https://github.com/xolox/vim-misc.git ~/.vim/xolox/vim-misc
+git clone https://github.com/xolox/vim-session.git ~/.vim/xolox/cim-session
+vim -u NONE -c "helptags ~/.vim/xolox/vim-session/doc" -c q
+
 # NERD TREE
-# git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 
 # VUE HIGHLIGHTS
-# git clone https://github.com/posva/vim-vue.git ~/.vim/pack/plugins/start/vim-vue
+git clone https://github.com/posva/vim-vue.git ~/.vim/pack/plugins/start/vim-vue
 
 # BUFFER EXPLORER
 vim -u NONE -c "helptags ~/.vim/bufexplorer-7.4.21/doc" -c q
 
-# SESSIONS
-vim -u NONE -c "helptags ~/.vim/vim-session/doc" -c q
+# VIM - AIRLINE
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+vim -u NONE -c "helptags ~/.vim/pack/dist/start/vim-airline" -c q
 
 echo 'End installation'
 #END INSTALL
--- vimrc
+++ vimrc
@@ -10,24 +10,13 @@ set runtimepath^=~/.vim/bufexplorer-7.4.21
 set runtimepath^=~/.vim/vim-l9
 set runtimepath^=~/.vim/vim-fuzzyfinder
 
-set runtimepath^=~/.vim/vim-scripts@vim-auto-save
+set runtimepath^=~/.vim/vim-scripts/vim-auto-save
 let g:auto_save=1
 
-set runtimepath^=~/.vim/xolox@vim-misc
-set runtimepath^=~/.vim/xolox@vim-session
+set runtimepath^=~/.vim/xolox/vim-misc
+set runtimepath^=~/.vim/xolox/vim-session
 let g:session_autosave='yes'
 let g:session_autoload='yes'
 let g:session_directory='./'
 set sessionoptions-=buffers
-
-
-"set runtimepath^=~./vim/molokai
-"let g:molokai_original = 1
-
-"set runtimepath^=~./vim/vim-auto-save
-"let g:auto_save = 1  " enable AutoSave on Vim startup
-
-" hola amigos
-" como estan
-" ahora yo voy a escribir algo aqui
 
